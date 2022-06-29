5d9440d8232e75eccccf56c0ebe6a6370be8c326
Noam Honig <noam.honig@gmail.com>
2022-06-29 17:42:28 +0300

    removed the initial data

--- src/backend/api.ts
+++ src/backend/api.ts
@@ -6,20 +6,5 @@ export const api = remultExpress({
     dataProvider: createPostgresConnection({
         connectionString: "postgres://postgres:MASTERKEY@localhost/postgres"
     }),
-    entities: [Task],
-    initApi: async remult => {
-        if (await remult.repo(Task).count() === 0) {
-            await remult.repo(Task).insert([
-                { id: 1, title: "Setup", completed: true },
-                { id: 2, title: "Entities", completed: false },
-                { id: 3, title: "Paging, Sorting and Filtering", completed: false },
-                { id: 4, title: "CRUD Operations", completed: false },
-                { id: 5, title: "Validation", completed: false },
-                { id: 6, title: "Backend methods", completed: false },
-                { id: 7, title: "Database", completed: false },
-                { id: 8, title: "Authentication and Authorization", completed: false },
-                { id: 9, title: "Deployment", completed: false }
-            ]);
-        }
-    }
+    entities: [Task]
 })
